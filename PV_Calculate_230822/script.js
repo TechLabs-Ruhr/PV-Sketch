993b26acdcb9f36e6f478790c9104e52250f4c83
Hannah C <70699831+HannahCordes@users.noreply.github.com>
2023-09-09 12:08:44 +0200

    Update script.js
    Test-Code Front-end für die Verbindung von Front- & Back-end

--- PV_Calculate_230822/script.js
+++ PV_Calculate_230822/script.js
@@ -31,3 +31,35 @@ function convertAddress() {
         })
         .catch(error => console.error("Error converting address:", error));
 }
+
+
+// test für front-end code für die verbindung von front-end und back-end
+
+document.getElementById("calculateButton").addEventListener("click", () => {
+    const selectedInclinationButton = document.querySelector(".toggle-button.active");
+    const selectedInclination = selectedInclinationButton.getAttribute("data-inclination");
+    
+    const data = {
+        postalcode: document.getElementById("postalCodeInput").value,
+        city: document.getElementById("cityInput").value,
+        street: document.getElementById("streetInput").value,
+        number: document.getElementById("numberInput").value,
+        roofinclination: selectedInclination
+    };
+    
+    fetch('/calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Display the calculation result in your front-end
+        document.getElementById("resultContainer").textContent = `Efficiency: ${result.efficiency * 100}%`;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
